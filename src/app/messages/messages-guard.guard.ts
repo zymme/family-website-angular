70a23625471a476f545c2f5dc3de3d09ea510e1d
Dave Zimmer <dave.zimmer@viawest.com>
2018-03-14 07:37:03 -0500

    Implemented guard service for messages and started web form for adding messages

--- src/app/messages/messages-guard.guard.ts
+++ src/app/messages/messages-guard.guard.ts
@@ -1,12 +1,22 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
 @Injectable()
-export class MessagesGuardGuard implements CanActivate {
+export class MessagesGuard implements CanActivate {
+
+  constructor(private _router: Router) {
+
+  }
+
+
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
     
-    return true;
+    if(localStorage.getItem('currentUser')) {
+      return true;
+    }
+    
+    this._router.navigate(['/login']);
   
   }
 }
--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MessagesComponent } from './messages.component';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { MessagesGuard } from './messages-guard.guard';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     RouterModule.forChild([
-      { path: 'messages', component: MessagesComponent }
+      { path: 'messages', component: MessagesComponent, 
+        canActivate: [MessagesGuard] }
     ])
   ],
   declarations: [MessagesComponent]
@@ -17,6 +19,9 @@ import { RouterModule } from '@angular/router';
   exports: [
     CommonModule,
     FormsModule
+  ],
+  providers: [
+    MessagesGuard
   ]
 })
 export class MessagesModule { }
